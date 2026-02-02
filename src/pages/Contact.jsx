23d1a51116c6a5694b66232c63047ae4d14586aa
TimvanOene01 <1355037@student.rocvantwente.nl>
2026-02-02 08:55:59 +0100

    kleur verandert linkjes gemaakt voor de maps en telefoonnummer

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     hero: {
       padding: "100px 20px",
       textAlign: "center",
-      borderBottom: "4px solid #c23939"
+      borderBottom: "4px solid #8B1A24"
     },
     redTitle: {
       color: "#000000",
@@ -26,11 +26,11 @@ const Contact = () => {
       letterSpacing: "3px",
     },
     infoCard: {
-      backgroundColor: "#c23939",
+      backgroundColor: "#8B1A24",
       border: "1px solid #333",
       borderRadius: "20px",
       padding: "40px",
-      borderTop: "5px solid #c23939"
+      borderTop: "5px solid #8B1A24"
     },
 
     data: {
@@ -56,13 +56,13 @@ const Contact = () => {
               <h2 className="mb-4">Direct contact</h2>
               
               <span style={styles.label}>Telefoon Studio</span>
-              <p style={styles.data}>📞 0800 - 123 2000</p>
+              <p style={styles.data}>📞 <a href="tel:+31800-1232000" style={{color: "white", textDecoration: "underline"}}>0800 - 123 2000</a></p>
 
               <span style={styles.label}>WhatsApp</span>
-              <p style={styles.data}>📱 +31 6 1234 5678</p>
+              <p style={styles.data}>📱 <a href="tel:+31612345678" style={{color: "white", textDecoration: "underline"}}>+31 6 1234 5678</a></p>
 
               <span style={styles.label}>E-mail</span>
-              <p style={styles.data}>✉️ radiovercel@gmail.com</p>
+              <p style={styles.data}>✉️ <a href="mailto:radiovercel@gmail.com" style={{color: "white", textDecoration: "underline"}}>radiovercel@gmail.com</a></p>
             </div>
           </div>
 
@@ -72,7 +72,7 @@ const Contact = () => {
               <h2 className="mb-4">Bezoekinformatie</h2>
               
               <span style={styles.label}>Locatie</span>
-              <p style={styles.data}>📍 Beeld & Geluid, Hilversum</p>
+              <p style={styles.data}>📍 <a href="https://www.google.com/maps/place/Nederlands+Instituut+voor+Beeld+%26+Geluid/@52.2354623,5.170482,508m/data=!3m2!1e3!4b1!4m6!3m5!1s0x47c66b5af17f1a73:0xd62b509149ca15ac!8m2!3d52.235459!4d5.1730569!16s%2Fm%2F03mbsnq?entry=ttu&g_ep=EgoyMDI2MDEyNS4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" style={{color: "white", textDecoration: "underline"}}>Beeld & Geluid, Hilversum</a></p>
 
               <span style={styles.label}>Uitzendperiode</span>
               <p style={styles.data}>🗓️ 25 t/m 31 december</p>
