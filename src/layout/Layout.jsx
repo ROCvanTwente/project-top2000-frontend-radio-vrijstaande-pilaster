99ffa6921035cbbe949c3caf9225b0017a4b6842
Luuk Velthuis <luuk.velthuis32@gmail.com>
2026-02-02 16:49:03 +0100

    Add async token fetching in Layout component for authentication

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,8 +3,16 @@ import { Outlet } from "react-router-dom";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { AlertProvider } from "../components/AlertContext";
+import { fetchApi } from "../components/ApiWrapper";
 
-const Layout = () => {
+const Layout = async () => {
+    const token = localStorage.getItem("token");
+    await fetchApi('https://radio-vrijstaande-pilaster.runasp.net/api/api/auth/me', {
+        headers: {
+            'Authorization': token ? `Bearer ${token}` : undefined,
+        },
+    }
+    )
     return (
         <AlertProvider>
             <Header />
